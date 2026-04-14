23b505ca8628d6ea7dd934042a6a34ff39887e3b
akshara-t <at6055@nyu.edu>
2026-04-14 12:11:58 -0400

    fixed issue

--- back-end/app.js
+++ back-end/app.js
@@ -123,12 +123,8 @@ app.post('/api/quiz/result', (req, res) => {
 });
 
 // Start server
-
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
-
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
 module.exports = app;
