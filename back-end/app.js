e4034dc48d9677aa85dceac2833ddeea04f5a010
akshara-t <at6055@nyu.edu>
2026-04-14 12:09:09 -0400

    added unit tests for 10% code coverage

--- back-end/app.js
+++ back-end/app.js
@@ -123,8 +123,12 @@ app.post('/api/quiz/result', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
 
 module.exports = app;
