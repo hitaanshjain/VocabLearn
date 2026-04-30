1dab52590aa66a669942bcc0c6745ebbad4003b9
saramhersch <smh9864@nyu.edu>
2026-04-30 08:29:42 -0400

    Restrict words to logged-in users

--- back-end/models/word.js
+++ back-end/models/word.js
@@ -15,6 +15,11 @@ const WordSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
 });
 
 export default mongoose.model('Word', WordSchema);
